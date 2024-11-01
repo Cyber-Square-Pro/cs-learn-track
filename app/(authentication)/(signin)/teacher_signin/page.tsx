d71e94319e75664ce4d198ab689d3d75901b98d5
Gopesh Janardhanan <68292507+Gopesh456@users.noreply.github.com>
2024-11-01 14:19:18 +0400

    checked build

--- app/(authentication)/(signin)/teacher_signin/page.tsx
+++ app/(authentication)/(signin)/teacher_signin/page.tsx
@@ -6,15 +6,6 @@ import Image from "next/image";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
 // fonts

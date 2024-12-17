69828730154aa895e63a84bbb865be699b463845
Gopesh Janardhanan <68292507+Gopesh456@users.noreply.github.com>
2024-12-17 15:44:05 +0400

    add utility functions and components for UI elements; update Tailwind configuration

--- app/(addStudent)/addStudent/page.tsx
+++ app/(addStudent)/addStudent/page.tsx
@@ -1,9 +1,300 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { Check, ChevronsUpDown } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
-const addStudent = () => {
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { error } from "console";
+import { errorToJSON } from "next/dist/server/render";
+
+const gender = [
+  { label: "Male", value: "male" },
+  { label: "Female", value: "female" },
+] as const;
+
+const formSchema = z.object({
+  firstName: z.string({ required_error: "" }),
+  secondName: z.string({ required_error: "" }),
+  grade: z.string({ required_error: "" }),
+  section: z.string({ required_error: "" }),
+  email: z
+    .string({ required_error: "" })
+    .email({ message: "Invalid Email id" }),
+  phone: z.string({ required_error: "" }).regex(/^05\d{7}$/, {
+    message: "Invalid UAE phone number.",
+  }),
+  gender: z.string({
+    required_error: "",
+  }),
+});
+
+const addstudentPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
-    <div>addStudent</div>
-  )
-}
+    <div className="bg-[#0C0C0C] container h-screen w-full text-[#FFFAFA] flex items-center justify-center">
+      <div className="container rounded-lg w-full max-w-md h-auto p-4 bg-[#17171a] mx-4 sm:mx-auto">
+        <h1 className="text-4xl font-bold text-left">Add a Student</h1>
+        <br />
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <div className="grid grid-cols-2 fit gap-4 ">
+              <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="First Name"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="secondName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Second Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Name of the Batch"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 fit gap-4 ">
+              <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="grade"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Grade</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Grade"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="section"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Section</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Name of the Batch"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 fit gap-4 ">
+              <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="gender"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel>Gender</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant="outline"
+                              role="combobox"
+                              className={cn(
+                                "w-[200px] justify-between text-white bg-transparent hover:bg-transparent hover:text-gray-500",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value
+                                ? gender.find(
+                                    (gender) => gender.value === field.value
+                                  )?.label
+                                : "Select the Gender"}
+                              <ChevronsUpDown className="opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-[200px] p-0">
+                          <Command>
+                            <CommandInput
+                              placeholder="Search framework..."
+                              className="h-9"
+                            />
+                            <CommandList>
+                              <CommandEmpty>No framework found.</CommandEmpty>
+                              <CommandGroup>
+                                {gender.map((gender) => (
+                                  <CommandItem
+                                    value={gender.label}
+                                    key={gender.value}
+                                    onSelect={() => {
+                                      form.setValue("gender", gender.value);
+                                    }}
+                                  >
+                                    {gender.label}
+                                    <Check
+                                      className={cn(
+                                        "ml-auto",
+                                        gender.value === field.value
+                                          ? "opacity-100"
+                                          : "opacity-0"
+                                      )}
+                                    />
+                                  </CommandItem>
+                                ))}
+                              </CommandGroup>
+                            </CommandList>
+                          </Command>
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              {/* <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="image"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Upload Image</FormLabel>
+                      <FormControl>
+                        <div className="relative w-full border-2 border-solid border-white rounded-lg flex items-center justify-center cursor-pointer hover:border-gray-600">
+                          <input
+                            type="file"
+                            accept="image/*"
+                            {...field}
+                            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                          />
+                          <span className="text-gray-500">
+                            Click to upload an image
+                          </span>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div> */}
+            </div>
+            <div className="grid grid-cols-2 fit gap-4 ">
+              <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Parent's Email"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="col-span-1">
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Parent's Phone Number"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="h-1"></div>
+            <div className="container grid place-content-end">
+              <Button
+                type="submit"
+                className="bg-[#0c0c0c] hover:bg-[#0c0c0c6c] font-[18px]"
+              >
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default addStudent
+export default addstudentPage;
